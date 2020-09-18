a5614fbc14ded23f5d95a079e3ccc5bae2cc0e82
Sean Fitzpatrick <sean.tfitz@icloud.com>
2020-09-18 11:39:27 +1000

    Updates

--- script.js
+++ script.js
@@ -2,60 +2,227 @@ let nav = (e)=>{
 	
 }
 
-// const queryStrings = {
-// 	"covid":{
-// 		"str":"covid"//4:3 - min-width: 700px; max-width: 900px;
-// 	},
-// 	"election":{
-// 		"str":"election"//1000 * 704
-// 	},
-// 	"tennis":{
-// 		"str":"tennis"//600 * 800
-// 	},
-// 	"tdf":{
-// 		"str":"tdf"//min-width: 670px - 1:1, or not...
-// 	},
-// 	"memory":{
-// 		"str":"memory"//630 / 744 = 84.67741935483871
-// 	},
-// 	"racing":{
-// 		"str":"racing"//max-height: 900px - 4:3, or not...
-// 	},
-// 	"season":{
-// 		"str":"season"//490 * 800
-// 	},
-// 	"grandfinal":{
-// 		"str":"grandfinal"//790 * 650
-// 	},
-// 	"nzelection":{
-// 		"str":"nzelection"//700 * 750
-// 	}
-// }
-
-const queryStrings = {
-	"covid": "//hosted.aap.com.au/interactives/covid19/index.html",
-	"election": "//hosted.aap.com.au/interactives/election_2019/houseofreps/index.html",
-	"tennis": "//hosted.aap.com.au/interactives/tennis_20/index.html?t=au",
-	"tdf": "//hosted.aap.com.au/interactives/tourdefrance_2016/index.html",
-	"memory": "//hosted.aap.com.au/interactives/memory/index.html?g=harry",
-	"racing": "//hosted.aap.com.au/interactives/springcarnival2019/index.html?domain=aap&meeting=mel",
-	"season": "//hosted.aap.com.au/interactives/nrl_19/index.html",
-	"grandfinal": "//hosted.aap.com.au/interactives/grandfinal_19/nrl/index.html",
-	"nzelection": "//hosted.aap.com.au/interactives/nzelection2014/stateoftheparliament/index.html",
+const details = {
+	"interactive":{
+		"covid": {
+			"url":"//hosted.aap.com.au/interactives/covid19/index.html",
+			"title":"COVID-19 Diary",
+			"description":"A daily snapshot of COVID-19 figures by Australian state and territory, and approximate worldwide figures. Beginning January 25 2020 and continuing to July 31 2020.<br><br>Includes image gallery with photos capturing significant moments and daily life during the pandemic.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"election": {
+			"url":"//hosted.aap.com.au/interactives/election_2019/houseofreps/index.html",
+			"title":"Federal Election",
+			"description":"Interactive graphic for the 2019 federal election. Displays the state of the parliament prior to, and following May 18 2019.<br><br>The pre-election 'Swing-O-Meter' simulates a swing of ten percent toward the government or opposition based on two-party-preferred margins. The yellow stars appearing on icons represent a seat won. Clicking on each icon reveals information about that electorate.<br><br>Each electorate can be selected from the drop-down menu, highlighting it's icon in the main section. The results were updated continuously on election night.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"tennis": {
+			"url":"//hosted.aap.com.au/interactives/tennis_20/index.html?t=au",
+			"title":"Australian Open",
+			"description":"An interactive graphic for the 2020 Australian Open tennis tournament.<br><br>Includes fixtures and results, profiles of selected top ranked and Australian players and grand slam singles finals records from 1968 to 2020.<br><br>Fixtures and match results updated via data feeds.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"tdf": {
+			"url":"//hosted.aap.com.au/interactives/tourdefrance_2016/index.html",
+			"title":"Tour De France",
+			"description":"An interactive guide to the stages of the 2016 Tour De France. The route of each stage is plotted out using the Google Maps API and shown with elevation profiles. Also featured is team information and race results.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"memory": {
+			"url":"//hosted.aap.com.au/interactives/memory/index.html?g=harry",
+			"title":"Memory Game",
+			"description":"A pair-matching memory game themed around royal couples, made in the lead up to Prince Harry and Meghan Markle's 2018 wedding.<br><br>The objective is to match the cards for the royal couples. The game can be played solo, against another person or against the computer, with two difficulty options.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"racing": {
+			"url":"//hosted.aap.com.au/interactives/springcarnival2019/index.html?domain=aap&meeting=mel",
+			"title":"Spring Racing",
+			"description":"Interactive graphic for the 2019 spring racing carnival. Includes form guide info for each race at eight meetings during the spring racing season.<br><br>Clicking each row expands it, revealing form guide info for each horse.<br><br>Also includes an interactive sweep form for the Melbourne Cup. The sweep form calculates prize pools and randomises draws and can be printed.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"season": {
+			"url":"//hosted.aap.com.au/interactives/nrl_19/index.html",
+			"title":"NRL Season Guide",
+			"description":"Interactive graphic for the 2019 NRL season. Featuring match fixtures, scores and details on each game. Also includes season ladder and team head-to-head comparisons.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"grandfinal": {
+			"url":"//hosted.aap.com.au/interactives/grandfinal_19/nrl/index.html",
+			"title":"NRL Grand Final",
+			"description":"Interactive graphic showing player stats & profiles in the 2019 Rugby League grand final. Scores & stats update following half time & full time via JSON data feed.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"nzelection": {
+			"url":"//hosted.aap.com.au/interactives/nzelection2014/stateoftheparliament/index.html",
+			"title":"New Zealand Election",
+			"description":"Infographic for the 2014 New Zealand election. The coloured hexagons represent seats won by Labour, National or crossbench candidates.<br><br>Clicking on coloured hexagons reveals inforation about that electorate. The hexagon design is to represent the inside of a beehive, to match the nickname of New Zealand's parliament house.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+	},
+	"infographic":{
+		"kyrgios-nadal":{
+			"title":"Kyrgios vs Nadal",
+			"description":"How Nick Kyrgios and Rafael Nadal compare ahead of their fourth-round Australian Open 2020 match on Monday January 27. Published 25/01/2020",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"melbourne-lockdown":{
+			"title":"Melbourne Lockdown",
+			"description":"A timeline of key events leading to Melbourne and Mitchell Shire to again be placed into stage-three lockdowns. Published 10/07/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"parliament":{
+			"title":"Parliamentary Leadership Changes",
+			"description":"Infographic showing changes to parliament as federal politicians return to Canberra for the first sitting week in 2020. Published 04/02/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"transport":{
+			"title":"NSW Public Transport",
+			"description":"The NSW government has revealed how public transport will run during COVID-19. Published 18/05/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"ABS_200505":{
+			"title":"Virus Households",
+			"description":"Inforaphic showing the financial impacts of COVID-19 on Australian households. Data released by Australian Bureau of Statistics May 5 2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"aviation":{
+			"title":"",
+			"description":"Inforaphic showing statistics for aviation incidents in Australia from 2010 to 2019 across commercial, general and recreational flying. Published 30/04/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"coal-power-stations":{
+			"title":"Coal Power Stations",
+			"description":"Infographic showing locations of Australia's largest coal power stations with chart displaying volumes of electricity generated by different sources. Published 12/02/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"COVID19-symptoms":{
+			"title":"COVID-19 Symtoms",
+			"description":"Graphic showing common symptom and rates of appearance in cases of Coronavirus COVID19. Based on information from the World Health Organization in early March 2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"COVIDHOTSPOTS-01":{
+			"title":"COVID-19 Hotspots, Victoria",
+			"description":"Map showing local council areas in Victoria considered COVID-19 hotspots in late June 2020. Published 22/06/2020",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"DST_ByState_Islands-01":{
+			"title":"Daylight Saving Time",
+			"description":"Graphic showing the difference in time zones across Australia. Includes changes for daylight saving time ending Sunday April 5 2020 and shows time zones for Australian island and Antarctic territories.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"budgetpreview_0722-01":{
+			"title":"Federal Budget Update",
+			"description":"Announcements and figures expected for the July 2020 economic and fiscal forecasts, ahead of a full federal budget in October. Published 22/07/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"nuclear-kimba":{
+			"title":"Nuclear Waste",
+			"description":"Map graphic showing locations of South Australian towns, Kimba and Wallerberdina in relation to Adelaide. Two sites are proposed for a nuclear waste dump near Kimba, on the Eyre Peninsula with another site in the Flinders Ranges being ruled out. Published 31/01/2020.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+	},
+	"video":{
+		"augusta": {
+			"title":"Augusta",
+			"description":"An animated hole by hole fly through of the Augusta golf course, created using Google Earth Studio.<br><br>The pin flags for each hole were then added using Adobe After Effects.",
+			"disclaimer":"",
+		},
+		"m4tunnel": {
+			"title":"M4 Tunnel Opening",
+			"description":"A video made using the custom <i>VidEx</i>&nbsp;application That I created for quick production of video, compiling still photography, footage and caption overlays.<br><br>Features animation created using Google Earth Studio, combined with overlays made with KML, showing the approximate route of the M4 tunnel between Homebush and Haberfield next to the existing route along Parramatta Road.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"apollo11": {
+			"title":"Apollo 11",
+			"description":"An animation created in Google Earth Studio for use in a video about the anniversary of the Apollo 11 mission.<br><br>The Saturn V rocket illustration was made in Adobe Illustrator, then animated and superimposed using Adobe After Effects.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"sharks": {
+			"title":"Sharks",
+			"description":"A video about shark related deaths in Australia.<br><br>Features animation created using Google Earth Studio. Animated location markers were added using Adobe After Effects.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+		"chineseship": {
+			"title":"Chinese Warship",
+			"description":"A video about the arrival of a Chinese warship in Sydney Harbour.<br><br>Features animation created using Google Earth Studio, showing the location of the Garden Island Naval Base in Sydney.",
+			"disclaimer":"displayed content &#169 Australian Associated Press",
+		},
+	},
+	"other":{
+		"honey_label":{
+			"title":"G&G's",
+			"description":"Label design for honey from an independent beekeeper.",
+		},
+		"oana":{
+			"title":"OANA",
+			"description":"Logo design for OANA. The Organization of Asia-Pacific News Agencies.",
+		},
+		"waxlogo":{
+			"title":"Wax Audio",
+			"description":"Logo design for <i>Wax Audio</i>, a musician and mash-up artist.<br><br>The microphone and headphones are arranged to resemble the <i>hammer and sickle</i> symbol.",
+		},
+		"bilt":{
+			"title":"BILT 1st",
+			"description":"Logos and business card design for a property business catering to Buyers, Investors, Landlords and Tenants.",
+		},
+		"RANZCRNIVAL":{
+			"title":"RANZCRNIVAL",
+			"description":"Poster promoting a social event at RANZCR (Royal Australian and New Zealand College of Radiologists).",
+		},
+		"invitation":{
+			"title":"Invitation",
+			"description":"Wedding invitation, place card and wishing well note.",
+		},
+		"zukos":{
+			"title":"Zuko's",
+			"description":"Logo and yardage for a 1950s style clothing store shown on a gusseted shopping bag.",
+		},
+		"dce":{
+			"title":"DCE",
+			"description":"Poster promoting an open day event at Design Centre Enmore.",
+		},
+		"tropdoc":{
+			"title":"TropDoc",
+			"description":"Poster promoting a short documentary film festival, the theme for the year being travel.",
+		},
+	}
 }
 
 const thumbPanels = document.getElementsByClassName('thumbnails');
 let thumbsArray = [];
 
-for(let i = 0; i < thumbPanels.length; i++){
-	thumbsArray.push(thumbPanels[i].id);
-	if(i == 0){
-		$('nav').append(`<button class="navBtn active" name="${thumbPanels[i].id}">${thumbPanels[i].id.replace(/_/g,'&nbsp;')}</button>`);
-	}else{
+// const start = ()=>{
+
+	for(let i = 0; i < thumbPanels.length; i++){
+		thumbsArray.push(thumbPanels[i].id);
+		// if(i == 0){
+		// 	$('nav').append(`<button class="navBtn active" name="${thumbPanels[i].id}">${thumbPanels[i].id.replace(/_/g,'&nbsp;')}</button>`);
+		// }else{
+		// 	$('nav').append(`<button class="navBtn" name="${thumbPanels[i].id}">${thumbPanels[i].id.replace(/_/g,'&nbsp;')}</button>`);
+		// }
 		$('nav').append(`<button class="navBtn" name="${thumbPanels[i].id}">${thumbPanels[i].id.replace(/_/g,'&nbsp;')}</button>`);
 	}
-}
 
+	if(typeof(Storage) !== "undefined" && localStorage['active']){
+		$('.thumbnails, .navBtn').removeClass('active');
+		$(`.navBtn[name="${localStorage['active']}"]`).addClass('active');
+		let matched = false;
+		for(let i in thumbsArray){
+			if(thumbsArray[i] == localStorage['active']){//it's the selected one
+				matched = true;
+				$(`#${thumbsArray[i]}`).addClass('active').removeClass('offLeft previousActive');
+			}else{
+				if(!matched){//comes before the selected one, move it to the left
+					$(`#${thumbsArray[i]}`).addClass('offLeft');	
+				}
+			}
+		}
+	}else{
+		$('#interactive, .navBtn[name="interactive"]').addClass('active');
+	}
+// };
+
+// localStorage.removeItem('active')
 
 $('.navBtn').on('click',(e)=>{
 	let matched = false;
@@ -88,6 +255,10 @@ $('.navBtn').on('click',(e)=>{
 
 	$('.navBtn').removeClass('active');
 	$(e.target).addClass('active');
+	
+	if(typeof(Storage) !== "undefined"){
+		localStorage.setItem('active',e.target.name);
+	}
 })
 
 $('.thumb').on('click',(e)=>{
@@ -104,13 +275,30 @@ $('.thumb').on('click',(e)=>{
 			<div class="containerOuter" id="container_${e.target.name}">
 				<div class="containerInner">
 					<div class="loading_icon"></div>
-					<iframe frameborder="0" scrolling="no" src="${queryStrings[e.target.name]}"></iframe>
+					<iframe frameborder="0" scrolling="no" src="${details[section][e.target.name]['url']}"></iframe>
 				</div>
 				<div class="infoBtn"></div>
 				<div class="infoText">
-					I'm baby tumeric pitchfork tattooed heirloom photo booth portland thundercats lumbersexual locavore cray letterpress poutine. Salvia master cleanse pabst, DIY four loko chia shaman direct trade tousled flexitarian vegan. Activated charcoal prism microdosing intelligentsia, photo booth celiac authentic yuccie vaporware deep v. Selvage man bun pug, deep v hella vice photo booth hexagon beard. Listicle blog aesthetic artisan direct trade wayfarers vaporware. IPhone fanny pack tumblr pok pok jean shorts echo park. Art party occupy tousled tacos dreamcatcher.
+					<h1>${details[section][e.target.name]['title']}</h1>
+					<p>${details[section][e.target.name]['description']}</p>
 				</div>
 				<button class="back">back</button>
+				<div class="disclaimer">${details[section][e.target.name]['disclaimer']}</div>
+			</div>
+		`)
+		break;
+
+		case 'infographic':
+		$('#preview').html(`
+			<div class="containerOuter">
+				<img class="infographic" draggable="false" src="infographic/${e.target.name}.jpg">
+				<div class="infoBtn"></div>
+				<div class="infoText">
+					<h1>${details[section][e.target.name]['title']}</h1>
+					<p>${details[section][e.target.name]['description']}</p>
+				</div>
+				<button class="back">back</button>
+				<div class="disclaimer">${details[section][e.target.name]['disclaimer']}</div>
 			</div>
 		`)
 		break;
@@ -123,7 +311,27 @@ $('.thumb').on('click',(e)=>{
 				</video>
 				<div class="infoBtn"></div>
 				<div class="infoText">
-					I'm baby tumeric pitchfork tattooed heirloom photo booth portland thundercats lumbersexual locavore cray letterpress poutine. Salvia master cleanse pabst, DIY four loko chia shaman direct trade tousled flexitarian vegan. Activated charcoal prism microdosing intelligentsia, photo booth celiac authentic yuccie vaporware deep v. Selvage man bun pug, deep v hella vice photo booth hexagon beard. Listicle blog aesthetic artisan direct trade wayfarers vaporware. IPhone fanny pack tumblr pok pok jean shorts echo park. Art party occupy tousled tacos dreamcatcher.
+					<h1>${details[section][e.target.name]['title']}</h1>
+					<p>${details[section][e.target.name]['description']}</p>
+				</div>
+				<button class="back">back</button>
+				<div class="disclaimer">${details[section][e.target.name]['disclaimer']}</div>
+			</div>
+		`)
+		break;
+
+		case 'other':
+		let ext = 'jpg';
+		if(e.target.name == 'bilt'){
+			ext = 'gif';
+		}
+		$('#preview').html(`
+			<div class="containerOuter">
+				<img class="other" draggable="false" src="other/${e.target.name}.${ext}">
+				<div class="infoBtn"></div>
+				<div class="infoText">
+					<h1>${details[section][e.target.name]['title']}</h1>
+					<p>${details[section][e.target.name]['description']}</p>
 				</div>
 				<button class="back">back</button>
 			</div>
@@ -144,3 +352,5 @@ $('.thumb').on('click',(e)=>{
 		}
 	})
 })
+
+// start();
